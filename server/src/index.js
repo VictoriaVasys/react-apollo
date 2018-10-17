af604c1978d1ab95e3ed5713034a045c19bd4635
Josh Nielsen <josh@boulderinvestment.tech>
2018-10-17 00:08:24 -0600

    updating server dir to use local docker

--- server/src/index.js
+++ server/src/index.js
@@ -21,7 +21,7 @@ const server = new GraphQLServer({
     ...req,
     db: new Prisma({
       typeDefs: 'src/generated/prisma.graphql',
-      endpoint: '__PRISMA_ENDPOINT__',
+      endpoint: 'http://localhost:4466',
       secret: 'mysecret123',
       debug: true,
     }),
